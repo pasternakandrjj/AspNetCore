f695b54b95910708683af5dfc44fa62a0694da81
pasternakandrjj <pasternakandrjj@gmail.com>
2020-01-22 12:00:49 +0200

    added swagger

--- Levi/Startup.cs
+++ Levi/Startup.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 using Levi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging; 
 
 namespace Levi
 {
@@ -30,6 +30,11 @@ namespace Levi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
 
             services.AddSingleton<ITaskServices, TaskService>();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,9 +45,26 @@ namespace Levi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection(); 
+            app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthorization();
+
+            //var swaggerOptions = new SwaggerOptions();
+            //Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            //app.UseSwagger(option =>
+            //{
+            //    option.RouteTemplate = swaggerOptions.JsonRoute;
+            //});
+            app.UseSwagger();
+            app.UseSwaggerUI(c=>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            //app.UseSwaggerUI(option =>
+            //{
+            //    option.SwaggerEndpoint(swaggerOptions.UIEndpoint, swaggerOptions.Description);
+            //});
 
             app.UseEndpoints(endpoints =>
             {
